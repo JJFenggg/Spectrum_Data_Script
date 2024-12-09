5c374d4c6b753717f8c263803b480916bbf964a3
JJFeng <jjfeng317@gmail.com>
2024-12-09 17:54:43 +0800

    new function

--- main.py
+++ main.py
@@ -2,8 +2,73 @@ import os
 import pandas as pd
 
 
-def modify_csv_files():
+def create_folders():
+    try:
+        os.mkdir('data')
+        os.mkdir('copy')
+    except FileExistsError:
+        pass
 
+    input("#################################\n"
+          "data文件夹与copy文件夹已创建。\n"
+          "#################################\n")
+
+
+def copy_files():
+    for root, dirs, files in os.walk('./'):
+        for file in files:
+            if file.endswith('.csv') or file.endswith('.CSV'):
+                file_path = os.path.join(root, file)
+                with (open(file_path, 'r', encoding='utf-8') as old_file,
+                      open('./copy/' + file, mode='a+', encoding='utf-8') as new_file):
+                    lines = old_file.readlines()
+                    for line in lines:
+                        new_file.write(line)
+
+    input("#################################\n"
+          "所有的文件已备份到 copy 文件夹下\n"
+          "#################################\n")
+
+
+def delete_useless_files():
+    for root, dirs, files in os.walk('./'):
+        for file in files:
+            # 检查文件是否满足删除条件
+            if (
+                (file.endswith('.csv') or file.endswith('.CSV'))  # 是 CSV 文件
+                and root == './'                                  # 限制只读取当前目录下的 CSV 文件
+                and 'i' not in file                              # 文件名中不包含 'i'
+            ):
+                file_path = os.path.join(root, file)  # 构建文件路径
+                try:
+                    os.remove(file_path)  # 删除文件
+                    print(f"Deleted: {file_path}")
+                except Exception as e:
+                    print(f"Failed to delete {file_path}: {e}")
+
+    input("#################################\n"
+          "所有的无用文件已删除。\n"
+          "#################################\n")
+
+
+def delete_useless_lines(lines_number=28):
+    for root, dirs, files in os.walk('./'):
+        for file in files:
+            if file.endswith('.csv') or file.endswith('.CSV'):
+                file_path = os.path.join(root, file)
+                with (open(file_path, 'r', encoding='utf-8') as old_file,
+                      open('./data/' + file, mode='a+', encoding='utf-8') as new_file):
+                    lines = old_file.readlines()
+                    for index, line in enumerate(lines):
+                        if index > lines_number:
+                            new_file.write(line)
+
+    input("#################################\n"
+          "文件的前30行已删除，并保存在data文件夹下\n"
+          "#################################\n")
+
+
+def modify_csv_files():
     result = pd.DataFrame()
 
     for root, dirs, files in os.walk('./data'):
@@ -44,24 +109,23 @@ def modify_csv_files():
             result[col] = result[col] / result[bg_column_name]
 
     result.to_csv('result.csv', index=False, encoding='utf-8')  # 将合并后的数据保存到 result.csv 文件中
+
     input("#################################\n"
-          "文件已经保存到本目录下的 'result.csv。\n"
+          "文件已经保存到本目录下的 'result。\n"
           "按 'Enter' 退出。\n"
           "#################################\n")
 
 
 def main():
     input("#################################\n"
-          "请把所有的 CSV 文件放到 data 文件夹下\n"
-          "并把背景以 'bg.csv' 命名。\n"
+          "请把背景以 'bg' 命名。\n"
           "然后按 'Enter' 继续。\n"
           "#################################\n")
-    try:
-        modify_csv_files()
-
-    except Exception as e:
-        print(f"Error: {e}")
-        input("按 'Enter' 退出。\n")
+    create_folders()
+    copy_files()
+    delete_useless_files()
+    delete_useless_lines()
+    modify_csv_files()
 
 
 if __name__ == "__main__":
